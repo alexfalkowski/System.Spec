26f8a99884f8cb9328317b14faefa080eea33dd6
Alex Falkowski <alexrfalkowski@gmail.com>
2012-06-03 14:05:14 +1000

    Added some ignore items and a spec.

---
+++ NSpec.Specs/SpecificationSpecs.cs
@@ -0,0 +1,28 @@
+﻿namespace NSpec.Specs
+{
+    using System;
+
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SpecificationSpecs
+    {
+        [Test]
+        public void ShouldGetMethodsFromAction()
+        {
+            var specification = new Specification();
+
+            Action action = () => specification.Describe("test");
+        }
+
+        private class Specification
+        {
+            public void Describe(string description)
+            {
+                
+            }
+        }
+    }
+}
