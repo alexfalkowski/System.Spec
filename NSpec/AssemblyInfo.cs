ff9cadb562ccfcefaab8d679fe3ffbe6d18c51fb
Alex Falkowski <alexrfalkowski@gmail.com>
2013-02-15 23:54:11 +1100

    Renaming the project to System.Spec

--- NSpec/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("NSpec")]
-[assembly: AssemblyDescription("NSpec is testing tool for the C# programming language.")]
-[assembly: AssemblyCompany("alex.falkowski")]
-[assembly: AssemblyProduct("NSpec")]
-[assembly: AssemblyCopyright("2013")]
-[assembly: AssemblyVersion("1.0.0")]
